2a58234a1e7cfcae16ab95f24d542cb30ed49a96
AlexRPADev <66978272+AlexRPADev@users.noreply.github.com>
2020-10-04 09:18:46 -0700

    Add files via upload

---
+++ NasaAppsChallenge/NasaSpaceApp.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace NasaAppsChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleUp = new System.Windows.Forms.Label();
+            this.earth = new System.Windows.Forms.Label();
+            this.proximaCentauri = new System.Windows.Forms.Label();
+            this.bttnEarthProCent = new System.Windows.Forms.Button();
+            this.displayEarthProCent = new System.Windows.Forms.Label();
+            this.galacticCenter = new System.Windows.Forms.Label();
+            this.bttnEarthGalacticCenter = new System.Windows.Forms.Button();
+            this.displayEarthGalacticCenter = new System.Windows.Forms.Label();
+            this.barnardsStar = new System.Windows.Forms.Label();
+            this.bttnEarthBarnardsStar = new System.Windows.Forms.Button();
+            this.displayEarthBarnardsStar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleUp
+            // 
+            this.titleUp.AutoSize = true;
+            this.titleUp.Font = new System.Drawing.Font("Garamond", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleUp.Location = new System.Drawing.Point(50, 19);
+            this.titleUp.Name = "titleUp";
+            this.titleUp.Size = new System.Drawing.Size(734, 27);
+            this.titleUp.TabIndex = 1;
+            this.titleUp.Text = "How fast will you travel through Universe using warp drive propulsion?";
+            // 
+            // earth
+            // 
+            this.earth.AutoSize = true;
+            this.earth.Font = new System.Drawing.Font("Garamond", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.earth.Location = new System.Drawing.Point(377, 212);
+            this.earth.Name = "earth";
+            this.earth.Size = new System.Drawing.Size(56, 21);
+            this.earth.TabIndex = 2;
+            this.earth.Text = "Earth";
+            // 
+            // proximaCentauri
+            // 
+            this.proximaCentauri.AutoSize = true;
+            this.proximaCentauri.Font = new System.Drawing.Font("Garamond", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.proximaCentauri.Location = new System.Drawing.Point(645, 313);
+            this.proximaCentauri.Name = "proximaCentauri";
+            this.proximaCentauri.Size = new System.Drawing.Size(155, 21);
+            this.proximaCentauri.TabIndex = 3;
+            this.proximaCentauri.Text = "Proxima Centauri";
+            // 
+            // bttnEarthProCent
+            // 
+            this.bttnEarthProCent.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnEarthProCent.Location = new System.Drawing.Point(480, 228);
+            this.bttnEarthProCent.Name = "bttnEarthProCent";
+            this.bttnEarthProCent.Size = new System.Drawing.Size(159, 85);
+            this.bttnEarthProCent.TabIndex = 4;
+            this.bttnEarthProCent.Text = "Calculate travel time from Earth to Proxima Centauri";
+            this.bttnEarthProCent.UseVisualStyleBackColor = true;
+            this.bttnEarthProCent.Click += new System.EventHandler(this.bttnEarthProCent_Click);
+            // 
+            // displayEarthProCent
+            // 
+            this.displayEarthProCent.AutoSize = true;
+            this.displayEarthProCent.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayEarthProCent.Location = new System.Drawing.Point(520, 316);
+            this.displayEarthProCent.Name = "displayEarthProCent";
+            this.displayEarthProCent.Size = new System.Drawing.Size(0, 18);
+            this.displayEarthProCent.TabIndex = 5;
+            this.displayEarthProCent.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // galacticCenter
+            // 
+            this.galacticCenter.AutoSize = true;
+            this.galacticCenter.Font = new System.Drawing.Font("Garamond", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.galacticCenter.Location = new System.Drawing.Point(23, 79);
+            this.galacticCenter.Name = "galacticCenter";
+            this.galacticCenter.Size = new System.Drawing.Size(136, 21);
+            this.galacticCenter.TabIndex = 6;
+            this.galacticCenter.Text = "Galactic Center";
+            // 
+            // bttnEarthGalacticCenter
+            // 
+            this.bttnEarthGalacticCenter.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnEarthGalacticCenter.Location = new System.Drawing.Point(161, 103);
+            this.bttnEarthGalacticCenter.Name = "bttnEarthGalacticCenter";
+            this.bttnEarthGalacticCenter.Size = new System.Drawing.Size(165, 77);
+            this.bttnEarthGalacticCenter.TabIndex = 7;
+            this.bttnEarthGalacticCenter.Text = "Calculate travel time from Earth to Galactic Center";
+            this.bttnEarthGalacticCenter.UseVisualStyleBackColor = true;
+            this.bttnEarthGalacticCenter.Click += new System.EventHandler(this.bttnEarthGalacticCenter_Click);
+            // 
+            // displayEarthGalacticCenter
+            // 
+            this.displayEarthGalacticCenter.AutoSize = true;
+            this.displayEarthGalacticCenter.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayEarthGalacticCenter.Location = new System.Drawing.Point(207, 187);
+            this.displayEarthGalacticCenter.Name = "displayEarthGalacticCenter";
+            this.displayEarthGalacticCenter.Size = new System.Drawing.Size(0, 18);
+            this.displayEarthGalacticCenter.TabIndex = 8;
+            // 
+            // barnardsStar
+            // 
+            this.barnardsStar.AutoSize = true;
+            this.barnardsStar.Font = new System.Drawing.Font("Garamond", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.barnardsStar.Location = new System.Drawing.Point(23, 353);
+            this.barnardsStar.Name = "barnardsStar";
+            this.barnardsStar.Size = new System.Drawing.Size(127, 21);
+            this.barnardsStar.TabIndex = 9;
+            this.barnardsStar.Text = "Barnard\'s Star";
+            this.barnardsStar.UseWaitCursor = true;
+            // 
+            // bttnEarthBarnardsStar
+            // 
+            this.bttnEarthBarnardsStar.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnEarthBarnardsStar.Location = new System.Drawing.Point(156, 265);
+            this.bttnEarthBarnardsStar.Name = "bttnEarthBarnardsStar";
+            this.bttnEarthBarnardsStar.Size = new System.Drawing.Size(174, 88);
+            this.bttnEarthBarnardsStar.TabIndex = 10;
+            this.bttnEarthBarnardsStar.Text = "Calculate travel time from Earth to Barnard\'s Star";
+            this.bttnEarthBarnardsStar.UseVisualStyleBackColor = true;
+            this.bttnEarthBarnardsStar.Click += new System.EventHandler(this.bttnEarthBarnardsStar_Click);
+            // 
+            // displayEarthBarnardsStar
+            // 
+            this.displayEarthBarnardsStar.AutoSize = true;
+            this.displayEarthBarnardsStar.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayEarthBarnardsStar.Location = new System.Drawing.Point(197, 356);
+            this.displayEarthBarnardsStar.Name = "displayEarthBarnardsStar";
+            this.displayEarthBarnardsStar.Size = new System.Drawing.Size(0, 18);
+            this.displayEarthBarnardsStar.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayEarthBarnardsStar);
+            this.Controls.Add(this.bttnEarthBarnardsStar);
+            this.Controls.Add(this.barnardsStar);
+            this.Controls.Add(this.displayEarthGalacticCenter);
+            this.Controls.Add(this.bttnEarthGalacticCenter);
+            this.Controls.Add(this.galacticCenter);
+            this.Controls.Add(this.displayEarthProCent);
+            this.Controls.Add(this.bttnEarthProCent);
+            this.Controls.Add(this.proximaCentauri);
+            this.Controls.Add(this.earth);
+            this.Controls.Add(this.titleUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Nasa Space App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label titleUp;
+        private System.Windows.Forms.Label earth;
+        private System.Windows.Forms.Label proximaCentauri;
+        private System.Windows.Forms.Button bttnEarthProCent;
+        private System.Windows.Forms.Label displayEarthProCent;
+        private System.Windows.Forms.Label galacticCenter;
+        private System.Windows.Forms.Button bttnEarthGalacticCenter;
+        private System.Windows.Forms.Label displayEarthGalacticCenter;
+        private System.Windows.Forms.Label barnardsStar;
+        private System.Windows.Forms.Button bttnEarthBarnardsStar;
+        private System.Windows.Forms.Label displayEarthBarnardsStar;
+    }
+}
+
---
+++ NasaAppsChallenge/NasaSpaceApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NasaAppsChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnEarthProCent_Click(object sender, EventArgs e)
+        {
+            int travelTimeEarthProCent = (497*286770)/86400;
+            displayEarthProCent.Text = travelTimeEarthProCent.ToString("n0")+" days";
+        }
+
+        private void bttnEarthGalacticCenter_Click(object sender, EventArgs e)
+        {
+            int travelTimeEarthGalCent = (497 * 1680000) / 86400;
+            displayEarthGalacticCenter.Text = travelTimeEarthGalCent.ToString("n0") + " days";            
+        }
+
+        private void bttnEarthBarnardsStar_Click(object sender, EventArgs e)
+        {
+            int travelTimeEarthBarnardsStar = (497 * 383586) / 86400;
+            displayEarthBarnardsStar.Text = travelTimeEarthBarnardsStar.ToString("n0") + " days";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
